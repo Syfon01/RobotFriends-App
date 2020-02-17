56a250650fa91ffd9ecf2b4e4e0f3467abdaeada
Sifon Isaac <syfonisaac@gmail.com>
2020-02-17 12:34:02 +0100

    image sizing

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({id, name, username, email}) => {
   return (
     < div className = "bg-light-green br3 grow pa3 ma2 dib shadow-2" >
-      < img src={`https://robohash.org/test ${id}?200x200`}
+      < img className="img-size" src={`https://robohash.org/test ${id}?200x200`}
       alt = "robo" / >
       <div>
         <h3>Name: {name}</h3>
