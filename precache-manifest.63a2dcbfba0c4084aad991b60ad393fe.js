717d2117d592a6b5ccbf125f2ea68231eecbf284
Sifon Isaac <syfonisaac@gmail.com>
2020-02-18 11:16:22 +0100

    Updates

---
+++ precache-manifest.63a2dcbfba0c4084aad991b60ad393fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4466ff441626a468bcc8fb500aa6004c",
+    "url": "/RobotFriends-App/index.html"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/RobotFriends-App/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "03c410de263dfe46b34b",
+    "url": "/RobotFriends-App/static/css/main.f98622ae.chunk.css"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/RobotFriends-App/static/js/2.57b720db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RobotFriends-App/static/js/2.57b720db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c410de263dfe46b34b",
+    "url": "/RobotFriends-App/static/js/main.cfa919ed.chunk.js"
+  },
+  {
+    "revision": "0844dbe09d017918039d",
+    "url": "/RobotFriends-App/static/js/runtime-main.d0c48d19.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotFriends-App/static/media/SEGA.12a3db8b.woff"
+  }
+]);
