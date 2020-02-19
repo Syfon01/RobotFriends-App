e1a35d1cc9c798e7234411fcfaea354abe29eadf
Sifon Isaac <syfonisaac@gmail.com>
2020-02-19 07:25:26 +0100

    redux initiation

---
+++ src/reducers.js
@@ -0,0 +1,15 @@
+import { CHANGE_SEARCHFIELD } from './constants.js'
+
+const initialState = {
+  searchField = ''
+}
+
+export const searchRobot = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case: CHANGE_SEARCHFIELD:
+      return Object.assign({}, state, searchField: action.payload);
+  
+    default:
+      return state
+  }
+}
